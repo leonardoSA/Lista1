f735d4a481e0b6248f1bcc808a1f024135b50260
leonardoSA <leocrazy007@hotmail.com>
2014-04-07 01:41:39 -0300

    first commit

---
+++ Exer_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	
+	char C;
+	
+	printf("Digite um caractere\n");
+	scanf("%c",&C);
+	printf("O Caractere %c, corresponde a %d na tabela ASCII\n", C, C);
+	
+	return 0;
+}
